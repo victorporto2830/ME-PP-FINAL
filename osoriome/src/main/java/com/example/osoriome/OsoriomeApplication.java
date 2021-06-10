46ecf9da165b6cd6c4bb019db8b42acf460fa5dd
victorporto2830 <83618124+victorporto2830@users.noreply.github.com>
2021-06-10 18:12:14 -0300

    Add files via upload

---
+++ osoriome/src/main/java/com/example/osoriome/OsoriomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.osoriome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsoriomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsoriomeApplication.class, args);
+	}
+
+}
